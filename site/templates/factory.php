7f06921728dc11991040ab8e53f7a1d284828324
Patrick Leisinger <patrick.leisinger@gmail.com>
2021-10-31 13:47:14 +0100

    [UPDATE] add factory pagination

--- site/templates/factory.php
+++ site/templates/factory.php
@@ -4,13 +4,13 @@
 <header class="relative">
   <?php if (!empty(param('tag'))): ?>
     <h1><?= html(urldecode(param('tag'))) ?></h1>
-    <?php $articles = $page->children()->filterBy('tags', urldecode(param('tag')), ','); ?>
+    <?php $articles = $page->children()->filterBy('tags', urldecode(param('tag')), ',')->paginate(1); ?>
   <?php else: ?>
     <div class="marginal-icon absolute grid place-items-center">
         <?= $page->icon(); ?>
     </div>    
     <h1><?= $page->title() ?></h1>
-    <?php $articles = $page->children()->listed() ?>
+    <?php $articles = $page->children()->listed()->paginate(1) ?>
     <p><?= $page->description() ?></p>
   <?php endif ?>
   <div>
@@ -34,5 +34,25 @@
     <?php endforeach ?>
   </div>
 </div>
+
+
+
+<?php if ($articles->pagination()->hasPages()): ?>
+<nav class="pagination">
+
+  <?php if ($articles->pagination()->hasNextPage()): ?>
+  <a class="next" href="<?= $articles->pagination()->nextPageURL() ?>">
+    ← older
+  </a>
+  <?php endif ?>
+
+  <?php if ($articles->pagination()->hasPrevPage()): ?>
+  <a class="prev" href="<?= $articles->pagination()->prevPageURL() ?>">
+    newer →
+  </a>
+  <?php endif ?>
+
+</nav>
+<?php endif ?>
 
 <?php snippet('footer') ?>
