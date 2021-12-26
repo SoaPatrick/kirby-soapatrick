b65dfac3f5d7b7b8ad922c774bf257ac6598909f
Patrick Leisinger <patrick.leisinger@gmail.com>
2021-12-26 15:57:46 +0100

    [UPDATE] add blog posts from 2003 and 2004
    - fix image block

--- site/snippets/blocks/image.php
+++ site/snippets/blocks/image.php
@@ -12,9 +12,11 @@ $id       = $block->_uid();
 
 if ($block->location() == 'web') {
     $src = $block->src()->esc();
+    $dimensions = '';
 } elseif ($image = $block->image()->toFile()) {
     $alt = $alt ?? $image->alt();
     $src = $image->url();
+    $dimensions = 'width="'.$image->width().'" height="'.$image->height().'"';
 }
 
 ?>
@@ -22,14 +24,14 @@ if ($block->location() == 'web') {
 <figure class="image-block" <?= attr(['data-ratio' => $ratio, 'data-crop' => $crop], ' ') ?>>
   <?php if ($link->isNotEmpty()): ?>
   <a href="<?= esc($link->toUrl()) ?>">
-    <img src="<?= $src ?>" alt="<?= $alt->esc() ?>" loading="lazy" width="<?= $image->width() ?>" height="<?= $image->height() ?>">
+    <img src="<?= $src ?>" alt="<?= $alt->esc() ?>" loading="lazy">
   </a>
   <?php elseif($lightbox): ?>
   <a href="<?= $src ?>" data-fslightbox="<?= $id ?>">
-    <img src="<?= $src ?>" alt="<?= $alt->esc() ?>" loading="lazy" width="<?= $image->width() ?>" height="<?= $image->height() ?>">
+    <img src="<?= $src ?>" alt="<?= $alt->esc() ?>" loading="lazy" <?= $dimensions ?>>
   </a>
   <?php else: ?>
-  <img src="<?= $src ?>" alt="<?= $alt->esc() ?>" loading="lazy" width="<?= $image->width() ?>" height="<?= $image->height() ?>">
+  <img src="<?= $src ?>" alt="<?= $alt->esc() ?>" loading="lazy" <?= $dimensions ?>>
   <?php endif ?>
 
   <?php if ($caption->isNotEmpty()): ?>
