8c1250b7f0be5f412708df95452cb4a408e697f4
Patrick Leisinger <patrick.leisinger@gmail.com>
2021-12-30 17:16:28 +0100

    [UPDATE] finish log items wirg icons

--- site/templates/log.php
+++ site/templates/log.php
@@ -1,5 +1,11 @@
 <?php snippet('header') ?>
-<h1><?= $page->title() ?></h1>
+<header class="relative">
+  <div class="marginal-icon marginal-icon--large mb-2 sm:mb-0 sm:absolute grid place-items-center">
+    <?= $page->icon(); ?>
+  </div>
+  <h1><?= $page->title() ?></h1>
+  <p class="text-lg"><?= $page->description() ?></p>
+</header>
 
 <?php
 $callback = function($p) {
@@ -8,9 +14,9 @@ $callback = function($p) {
 $groupedItems = $page->children()->listed()->flip()->group($callback);
 foreach($groupedItems as $day => $itemsPerDay): ?>
     <h2 class="capitalize"><?= $day ?></h2>
-    <ul>
+    <ul class="list-none mb-12 pl-6 leading-6">
       <?php foreach($itemsPerDay as $item) : ?>
-      <li><strong class="capitalize"><?= $item->type() ?></strong> <?= $item->title() ?></li>
+        <li class="relative"><?php snippet('log-icon', ['icon' => $item->type()]) ?><strong class="capitalize"><?= $item->type() ?></strong> <?= $item->title() ?></li>
       <?php endforeach; ?>
     </ul>
 <?php endforeach ?>
