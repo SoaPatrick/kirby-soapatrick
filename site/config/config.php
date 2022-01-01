68aaf7678cb2e86c4b16b3875521052a7c50953c
Patrick Leisinger <patrick.leisinger@gmail.com>
2022-01-01 15:20:47 +0100

    [UPDATE] add correct format to articles
    - add plugin for better list view
    - optimize blueprints
    - add panel customization

--- site/config/config.php
+++ site/config/config.php
@@ -4,7 +4,18 @@ return [
   'debug' => true,
   'optionApiUrl' => 'https://kirby.soapatrick.com/api/',
   'panel' => [
-    'slug' => 'yygang'
+    'slug' => 'yygang',
+    'css' => 'assets/css/custom-panel.css',
+    'favicon' => [
+      'apple-touch-icon' => [
+        'type' => 'image/png',
+        'url'  =>  'assets/img/apple-touch-icon.png',
+      ],
+      'shortcut icon' => [
+        'type' => 'image/svg+xml',
+        'url'  => 'assets/img/icon.svg',
+      ]
+    ]
   ],
   'routes' => [
     [
