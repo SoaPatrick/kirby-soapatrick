7796f8665090d540c079c46fd76e370d74cf4e92
Patrick Leisinger <patrick.leisinger@gmail.com>
2022-01-06 15:56:21 +0100

    SP-3 [ADD] add sitemap

--- site/config/config.php
+++ site/config/config.php
@@ -16,5 +16,31 @@ return [
         'url'  => 'assets/img/icon.svg',
       ]
     ]
-  ]
+  ],
+  'routes' => [
+    [
+      'pattern' => 'sitemap.xml',
+      'action'  => function() {
+          $pages = site()->pages()->index();
+
+          // fetch the pages to ignore from the config settings,
+          // if nothing is set, we ignore the error page
+          $ignore = kirby()->option('sitemap.ignore', ['error']);
+          $log = page('log')->children()->pluck('id', ',');
+          $lab = page('lab')->children()->pluck('id', ',');
+          $ignore = array_merge($ignore, $log, $lab);
+
+          $content = snippet('sitemap', compact('pages', 'ignore'), true);
+
+          return new Kirby\Cms\Response($content, 'application/xml');
+      }
+    ],
+    [
+      'pattern' => 'sitemap',
+      'action'  => function() {
+        return go('sitemap.xml', 301);
+      }
+    ]
+  ],
+  'sitemap.ignore' => ['log', 'factory', 'box'],
 ];
