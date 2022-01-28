cdefa0a12f92769c28dbe5792459afff847d6082
Patrick Leisinger <patrick.leisinger@gmail.com>
2022-01-28 19:26:52 +0100

    [UPDATE] improve subnav edit function

--- site/snippets/layouts/subnavigation.php
+++ site/snippets/layouts/subnavigation.php
@@ -1,5 +1,5 @@
 <nav class="sub-navigation">
-  <?php foreach(page('home')->$subnav()->split($separator = ',') as $article): ?>
+  <?php foreach(page('home')->$subnav()->toPages() as $article): ?>
       <a href="<?= page($article)->url() ?>" class="bold-link<?php e(page($article)->isOpen(), ' active') ?>"><?= $article ?> </a>
   <?php endforeach ?>  
 </nav>
